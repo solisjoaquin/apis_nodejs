d758cb3e095c4c6f41a845f98735a8d9ea9437bf
Joaquin <sol19008@byui.edu>
2020-12-10 10:42:00 -0300

    adding products route

--- README.md
+++ README.md
@@ -10,3 +10,14 @@ npm init -y
 npm install express body-parser
 ```
 
+instalar nodemon
+
+```
+npm install --save-dev nodemon
+```
+
+correr el proyecto en puerto 3000
+
+```
+node index.js
+```
--- index.js
+++ index.js
@@ -3,6 +3,25 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+const productos = [
+    { id: '123', titulo: 'macbook', precio: 1300, moneda: 'USD' },
+    { id: '123', titulo: 'macbook', precio: 1300, moneda: 'USD' },
+    { id: '123', titulo: 'macbook', precio: 1300, moneda: 'USD' },
+]
+
+app.route('/productos').get((req, res) => { res.json(productos) })
+
+app.get('/productos/:id', (req, res) => {
+    for (producto of productos) {
+        if (producto.id == req.params.id) {
+            res.json(producto)
+            return
+        }
+    }
+    res.status(404).send(`El producto con id[${req.params.id}] no existe`)
+})
+
+
 app.get('/', (req, res) => {
     res.send('API activa')
 })
